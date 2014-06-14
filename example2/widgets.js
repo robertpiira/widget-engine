cdd81d476cd840f483acf110ca1928a665a9052a
robertpiira <robert@piira.se>
2014-06-15 01:33:25 +0200

    removed the backbone-like view idea and added an example3

--- example2/widgets.js
+++ example2/widgets.js
@@ -1,27 +1,30 @@
 'use strict';
 
-$we.register('qsFruits', ['bower_components/underscore/underscore.js', 'bower_components/jquery/dist/jquery.min.js', 'css/banana.css'], function () {
+$we.register('qsFruits', ['/example2/bower_components/underscore/underscore.js', '/example2/bower_components/jquery/dist/jquery.min.js', '/example2/css/banana.css'], function () {
 
-  var templateUrl = 'templates/fruits.tpl';
+  var templateUrl = '/example2/templates/fruits.tpl';
   var widgets = document.querySelectorAll('[data-widget-type="fruitget"]');
 
-  var fruitView = $we.View({
+  var FruitWidget = function FruitWidget(el) {
 
-    initialize: function () {
+    this.el = el;
+    this.$el = $(this.el);
+
+    this.initialize = function () {
 
       this.events();
       this.render();
 
-    },
+    };
 
-    events: function () {
+    this.events = function () {
 
       this.$el.on('click', '.js-reco-btn', $.proxy(this.pickFood, this));
       this.$el.on('click', '.js-btn', $.proxy(this.shuffleAndRender, this));
 
-    },
+    };
 
-    render: function () {
+    this.render = function () {
 
       var _this = this;
       var markup;
@@ -34,41 +37,41 @@ $we.register('qsFruits', ['bower_components/underscore/underscore.js', 'bower_co
 
           _this.$el.html(template);
 
-          markup = _this.$('#header-tpl').html();
-          _this.$('#header').html(_.template(markup, { title: _this.$el.attr('data-widget-title') } ));
+          markup = _this.$el.find('#header-tpl').html();
+          _this.$el.find('#header').html(_.template(markup, { title: _this.$el.attr('data-widget-title') } ));
 
         });
 
       });
 
+    };
 
-
-    },
-
-    shuffleAndRender: function () {
+    this.shuffleAndRender= function () {
 
       var shuffledData = _.shuffle(this.data);
-      var markup = $('#foods-tpl').html();
+      var markup = this.$el.find('#foods-tpl').html();
 
-      this.$('#foods').html(_.template(markup, { foods: shuffledData } ));
+      this.$el.find('#foods').html(_.template(markup, { foods: shuffledData } ));
 
-    },
+    };
 
-    pickFood: function () {
+    this.pickFood = function () {
 
         var favoIndex = _.random(0, this.data.length - 1);
         var favoFood = this.data[favoIndex];
 
         alert('the pick is: ' + favoFood);
 
-    }
+    };
 
-  });
+    this.initialize();
+
+  };
 
 
   _.each(widgets, function (widget) {
 
-    fruitView.bootstrap({ el: widget });
+    new FruitWidget(widget);
 
   });
 
